7453be2a029ab6b73f945a117092531562871b14
Affan Saleem <m.affansalim@gmail.com>
2024-03-21 17:27:02 +0500

    Small UI Change

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -3,8 +3,9 @@
 const Footer = () => {
   return (
     <footer className="fixed bottom-0 left-0 w-full bg-gray-800 text-white py-4 text-center">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between">
         <p>&copy; 2024 Your Company. All rights reserved.</p>
+        <p>Created By Affan Saleem</p>
       </div>
     </footer>
   );
